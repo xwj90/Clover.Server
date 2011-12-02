b80bfefc9946d5efc92b749c3a38f6669f721dbf
shiweichuan@gmail.com <shiweichuan@gmail.com>
2011-12-03 00:04:32 +0800

    hashtable interface

--- Source/Clover.ClientLibrary/Hashtable.cs
+++ Source/Clover.ClientLibrary/Hashtable.cs
@@ -7,8 +7,46 @@ namespace Clover.ClientLibrary
 {
     public class Hashtable : IDisposable
     {
-          
-
+        /// <summary>
+        /// Set the key with value
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Set(string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Set the key with value and expire
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="ts"></param>
+        public void SetWithExpire(string key, object value, TimeSpan ts)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Set the key with value only when the key hasn't modified by anyother
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="oldValue"></param>
+        /// <returns></returns>
+        public bool SetWithVersion(string key, object value, out object oldValue)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get the value associate with the key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public object Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+        
 
         public void Dispose()
         {
