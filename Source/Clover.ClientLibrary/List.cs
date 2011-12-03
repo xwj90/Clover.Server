b2fb381e9ff79ef063cf230453561c46e5c96b81
shiweichuan@gmail.com <shiweichuan@gmail.com>
2011-12-03 20:47:46 +0800

    add list interface

---
+++ Source/Clover.ClientLibrary/List.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clover.ClientLibrary
+{
+    public class List
+    {
+        public void Add(object value)
+        {
+        }
+        public void AddRange(IEnumerable<object> values)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Remove(object value)
+        {
+            throw new NotImplementedException();
+        }
+        public bool RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+        public void Clear()
+        {
+        }
+        public bool Contains(object value)
+        {
+            throw new NotImplementedException();
+        }
+        public int Count
+        {
+            get;
+        }
+        public object this[int index]
+        {
+            get;
+            set;
+        }
+    }
+}
