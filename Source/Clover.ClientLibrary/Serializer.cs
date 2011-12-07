4d43a4d9210e601a6bb9f7f8527b64ec0e8f7df3
xwj90 <xwj90@hotmail.com>
2011-12-07 08:20:41 +0800

    draft 5

---
+++ Source/Clover.ClientLibrary/Serializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Clover.ClientLibrary
+{
+    public  static class Serializer
+    {
+        public static byte[] SerializeHashtable(string hashtableName, string key, object value)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (StreamReader sr = new StreamReader(ms))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(ms, value);
+                   // ProtoBuf.Serializer.Serialize<object>(ms, value);
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ Source/UnitTest/SerializerTest.cs
@@ -0,0 +1,83 @@
+﻿using Clover.ClientLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for SerializerTest and is intended
+    ///to contain all SerializerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class SerializerTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for SerializeHashtable
+        ///</summary>
+        [TestMethod()]
+        public void SerializeHashtableTest()
+        {
+            string hashtableName = string.Empty; // TODO: Initialize to an appropriate value
+            string key = string.Empty; // TODO: Initialize to an appropriate value
+            object value = "111111111"; // TODO: Initialize to an appropriate value
+            byte[] expected = null; // TODO: Initialize to an appropriate value
+            byte[] actual;
+            actual = Serializer.SerializeHashtable(hashtableName, key, value);
+            Assert.AreEqual(expected, actual);
+           // Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
